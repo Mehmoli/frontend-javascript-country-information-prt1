7d07ecca914f393a99b11d7f1a1ac169b4da4110
Mehmoli <meagulec@gmail.com>
2023-02-21 17:15:54 +0100

    app.js totaal aangepast, probleem anders opgelost!

--- src/app.js
+++ src/app.js
@@ -1,64 +1,59 @@
 import axios from "axios";
 
-let myList = [];
-async function fetchData(){
+
+async function fetchData() {
     try {
-        const result = await axios.get('https://restcountries.com/v2/all')
-        console.log(result.data);
-        myList = result.data ;
+        const result = await axios.get('https://restcountries.com/v2/all');
 
-        myList.sort((a, b) =>{
-            return a.population - b.population});
+        const myList = result.data;
 
-        for (let i = 0; i < myList.length; i++) {
-            // console.log(myList[i].name);
-            // console.log(myList[i].region);
-            // console.log(myList[i].population);
+        myList.sort((a, b) => {
+            return a.population - b.population;
+        });
 
-            // console.log(myList[i].region);
-            let ul = document.getElementById('countries');
-            let li = document.createElement("li");
-            li.innerHTML = `<div id="listContainer">
-            <span id="flag"><img id="flagCountry" src="${myList[i].flag}" alt="flag country"/></span>
+        const ul = document.getElementById('countries');
+
+        const listOfWorldCountries = myList.map((listOfWorldCountry) => {
+            return  `<li><div id="listContainer">
+            <span id="flag"><img id="flagCountry" src="${listOfWorldCountry.flag}" alt="flag country"/></span>
             <div id="textContainer">
-            <p id="countryName">${myList[i].name}</p>
-            <p id="worldRegion">${myList[i].region}</p>
+            <p id="countryName" class="${listOfWorldCountry.region}">${listOfWorldCountry.name}</p>
+            <p id="worldRegion">${listOfWorldCountry.region}</p>
             </div>
             </div>
-            <p id="countryPopulation">Has a population of ${myList[i].population} people</p>
-            `;
-            ul.appendChild(li)
-            const colorNameCountry = document.getElementById('countryName');
-            let colorCountry = myList[i].region;
-            function colorNames() {
-                switch (colorCountry) {
-                    case 'Africa':
-                        colorNameCountry.style.color="blue";
-                        break;
-                    case 'Americas':
-                        console.log('green');
-                        break;
-                    case 'Asia':
-                        console.log('red');
-                        break;
-                    case 'Europe':
-                        console.log('yellow');
-                        break;
-                    case 'Oceania':
-                        console.log('purple');
-                        break;
-                    default:
-                        console.log('Unknown country region');
-                        break;
+            <p id="countryPopulation">Has a population of ${listOfWorldCountry.population} people</p>
+            </li>
+            `});
+
+        ul.innerHTML = listOfWorldCountries.join('');
+
+        const asiaColor = document.getElementsByClassName("Asia");
+        const africaColor = document.getElementsByClassName("Africa");
+        const americasColor = document.getElementsByClassName("Americas");
+        const europeColor = document.getElementsByClassName("Europe");
+        const oceaniaColor = document.getElementsByClassName("Oceania");
+
+
+                for (let i = 0; i < asiaColor.length; i++) {
+                asiaColor[i].style.color = 'red';
+                 }
+                for (let i = 0; i < africaColor.length; i++) {
+                    africaColor[i].style.color = 'blue';
                 }
-            }
-            colorNames(colorCountry);
-            colorNameCountry.style.color = "${colorNames}";
-        }
-    } catch (e){
-        console.error(e)
+                for (let i = 0; i < americasColor.length; i++) {
+                    americasColor[i].style.color = 'green';
+                }
+                for (let i = 0; i < europeColor.length; i++) {
+                    europeColor[i].style.color = '#F9DB24';
+                }
+                for (let i = 0; i < oceaniaColor.length; i++) {
+                    oceaniaColor[i].style.color = 'purple';
+                }
+
+    } catch (e) {
+        console.error('OOPS!!!!!')
+
     }
 }
 
 fetchData();
-

c20a17a1e9c6b5f6e8204bf7a685304a9482e8d2
Mehmoli <meagulec@gmail.com>
2023-03-01 20:56:36 +0100

    search.js nieuw aangemaakt voor zoek functie, index.html aangepast, en css.

--- src/app.js
+++ src/app.js
@@ -1,82 +1,61 @@
 import axios from "axios";
 
 
-// async function fetchData() {
-//     try {
-//         const result = await axios.get('https://restcountries.com/v2/all');
-//
-//         const myList = result.data;
-//
-//         myList.sort((a, b) => {
-//             return a.population - b.population;
-//         });
-//
-//         const ul = document.getElementById('countries');
-//
-//         const listOfWorldCountries = myList.map((listOfWorldCountry) => {
-//             return  `<li><div id="listContainer">
-//             <span id="flag"><img id="flagCountry" src="${listOfWorldCountry.flag}" alt="flag country"/></span>
-//             <div id="textContainer">
-//             <p id="countryName" class="${listOfWorldCountry.region}">${listOfWorldCountry.name}</p>
-//             <p id="worldRegion">${listOfWorldCountry.region}</p>
-//             </div>
-//             </div>
-//             <p id="countryPopulation">Has a population of ${listOfWorldCountry.population} people</p>
-//             </li>
-//             `});
-//
-//         ul.innerHTML = listOfWorldCountries.join('');
-//
-//         const asiaColor = document.getElementsByClassName("Asia");
-//         const africaColor = document.getElementsByClassName("Africa");
-//         const americasColor = document.getElementsByClassName("Americas");
-//         const europeColor = document.getElementsByClassName("Europe");
-//         const oceaniaColor = document.getElementsByClassName("Oceania");
-//
-//
-//                 for (let i = 0; i < asiaColor.length; i++) {
-//                 asiaColor[i].style.color = 'red';
-//                  }
-//                 for (let i = 0; i < africaColor.length; i++) {
-//                     africaColor[i].style.color = 'blue';
-//                 }
-//                 for (let i = 0; i < americasColor.length; i++) {
-//                     americasColor[i].style.color = 'green';
-//                 }
-//                 for (let i = 0; i < europeColor.length; i++) {
-//                     europeColor[i].style.color = '#F9DB24';
-//                 }
-//                 for (let i = 0; i < oceaniaColor.length; i++) {
-//                     oceaniaColor[i].style.color = 'purple';
-//                 }
-//
-//     } catch (e) {
-//         console.error('OOPS!!!!!')
-//
-//     }
-// }
-//
-// fetchData();
-
-const userInput = document.getElementById('user-input');
-console.log(userInput.value);
-
 async function fetchData() {
     try {
+        const result = await axios.get('https://restcountries.com/v2/all');
 
-        const result = await axios.get('https://restcountries.com/v2/name/netherlands');
-        const myList = result.data[0];
-        console.log(myList);
-        console.log(myList.name);
+        const myList = result.data;
 
-        const section = document.getElementById('country-card');
-        section.innerHTML = `<p>${myList.currencies[0].name}</p>
-                             <p>${myList.capital}</p>`;
+        myList.sort((a, b) => {
+            return a.population - b.population;
+        });
+
+        const ul = document.getElementById('countries');
+
+        const listOfWorldCountries = myList.map((listOfWorldCountry) => {
+            return  `<li><div id="listContainer">
+            <span id="flag"><img id="flagCountry" src="${listOfWorldCountry.flag}" alt="flag country"/></span>
+            <div id="textContainer">
+            <p id="countryName" class="${listOfWorldCountry.region}">${listOfWorldCountry.name}</p>
+            <p id="worldRegion">${listOfWorldCountry.region}</p>
+            </div>
+            </div>
+            <p id="countryPopulation">Has a population of ${listOfWorldCountry.population} people</p>
+            </li>
+            `});
+
+        ul.innerHTML = listOfWorldCountries.join('');
+
+        const asiaColor = document.getElementsByClassName("Asia");
+        const africaColor = document.getElementsByClassName("Africa");
+        const americasColor = document.getElementsByClassName("Americas");
+        const europeColor = document.getElementsByClassName("Europe");
+        const oceaniaColor = document.getElementsByClassName("Oceania");
 
 
-    } catch (e){
-        console.log(e)
+                for (let i = 0; i < asiaColor.length; i++) {
+                asiaColor[i].style.color = 'red';
+                 }
+                for (let i = 0; i < africaColor.length; i++) {
+                    africaColor[i].style.color = 'blue';
+                }
+                for (let i = 0; i < americasColor.length; i++) {
+                    americasColor[i].style.color = 'green';
+                }
+                for (let i = 0; i < europeColor.length; i++) {
+                    europeColor[i].style.color = '#F9DB24';
+                }
+                for (let i = 0; i < oceaniaColor.length; i++) {
+                    oceaniaColor[i].style.color = 'purple';
+                }
+
+    } catch (e) {
+        console.error('OOPS!!!!!')
+
     }
-
 }
+
 fetchData();
+
+
---
+++ src/search.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+//MAAK EEN VARIABELE AAN MET DE NAAM USERINPUT
+//HAAL DE GEGEVENS OP VAN USER-INPUT
+const userInput = document.getElementById('user-input');
+//MAAK EEN VARIABELE AAN MET DE NAAM SUBMITBTN
+//HAAL DE GEGEVENS OP VAN SUBMITBUTTON
+const submitBtn = document.getElementById('submitButton');
+//ZET EEN EVENTLISTENER OP CLICK VAN SUBMITBUTTON EN ACTIVEER FUNCTIE GETUSERINPUT
+submitBtn.addEventListener('click', getUserInput);
+
+//MAAK EEN FUNCTIE VOOR HET OPHALEN VAN EEN LAND
+function getUserInput(e) {
+    e.preventDefault();
+    getData(userInput.value);
+    userInput.value = '';
+}
+
+
+//MAAK EEN ASYNCHRONE FUNCTIE VOOR HET OPHALEN VAN DATA
+async function getData(searchName){
+    userInput.innerHTML = ``;
+    let searchResult;
+    try{
+            //MAAK EEN VARIABELE RESULT VOOR HET OPSLAAN VAN OPGEHAALDE DATA
+            const result = await axios.get(`https://restcountries.com/v2/name/${searchName}`);
+            // console.log(result.data);
+            searchResult = result.data[0];
+            //MAAK EEN VARIABELE VOOR COUNTRYCARD EN HAAL DE GEGEVENS OP
+            const countryCard = document.getElementById('country-card');
+
+            //ZET IN HTML IN VARRIABELE COUNTRYCARD DE INTERACTIVE ELEMENTEN
+            countryCard.innerHTML = `<img id="flagCountry" src="${searchResult.flag}" alt="flag country"/><span id="nameCountry"><h2>${searchResult.name}</h2></span>
+                             <hr>
+                             <p>${searchResult.name} is situated in ${searchResult.subregion}. It has a population of ${searchResult.population} people.</p>
+                             <p>The capital is ${searchResult.capital} ${currencyChecker(searchResult.currencies)}'s</p>
+                             <p>They speak ${searchResult.languages[0].name}</p>  
+                             `;
+
+    } catch (e){
+        //ALS SEARCHRESULT GEEN GELDIGE WARDE HEEFT
+        if (!searchResult) {
+            //MAAK EEN VARIABELE EN HAAL ELEMENT MET ID COUNTRY-CARD OP
+            const errorMessage = document.getElementById('country-card');
+            //ZET IN OPGEHAALDE ELEMENT EEN P ELEMENT MET TEXT
+            errorMessage.innerHTML = `<p class="errorMessage">There is no country with the name ${searchName.toUpperCase()}. You can try again..</p>`
+        }
+        console.log(e);
+
+    }
+}
+//MAAK EEN FUNCTIE DIE CHECKT OF ER IN EEN LAND TWEE VERSCHILLENDE CURRENCIES WORDEN GEBRUIKT
+function currencyChecker(currencies) {
+    let output = ' and you can pay with ';
+    if (currencies.length === 2) {
+        return output + `${currencies[0].name} and ${currencies[1].name}'s`;
+    } else {
+        return output + `${currencies[0].name}`;
+    }
+    
+}
